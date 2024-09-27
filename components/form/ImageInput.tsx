4af4b4e1bb1641851c13cd834ab70bd56f2238b3
tomoya <alligatorfree12@gmail.com>
2024-09-28 08:23:33 +0900

    80. Image Input

---
+++ components/form/ImageInput.tsx
@@ -0,0 +1,26 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+// 80. Image Input
+
+const ImageInput = () => {
+  const name = "image";
+
+  return (
+    <div className={"mb-2"}>
+      <Label htmlFor={name} className={"capitalize"}>
+        Image
+      </Label>
+      <Input
+        id={name}
+        name={name}
+        type={"file"}
+        required={true}
+        accept={"image/*"}
+        className={"max-w-xs"}
+      />
+    </div>
+  );
+};
+
+export default ImageInput;

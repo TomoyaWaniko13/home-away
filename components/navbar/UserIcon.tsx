be8fb9d281bd02b2b3032cfa958d98c6c29aad6d
tomoya <alligatorfree12@gmail.com>
2024-09-27 17:37:43 +0900

    74. Fetch Profile Image Action

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,19 @@
 import { LuUser2 } from "react-icons/lu";
+import { fetchProfileImage } from "@/utils/actions";
 
-const UserIcon = () => {
+const UserIcon = async () => {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        className={"w-6 h-6 rounded-full object-cover"}
+        alt={"user image"}
+      />
+    );
+  }
+
   return <LuUser2 className={"w-6 h-6 bg-primary rounded-full text-white"} />;
 };
 
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,8 +1,16 @@
 // 67. Zod Library
+// 73. Create Profile Model and createProfileAction
 
 "use server";
 
+import db from "@/utils/db";
+import { createClerkClient, currentUser } from "@clerk/nextjs/server";
 import { profileSchema } from "@/utils/schemas";
+import { redirect } from "next/navigation";
+
+const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 // form の submit の時に実行される server action です。
 export const createProfileAction = async (
@@ -10,13 +18,51 @@ export const createProfileAction = async (
   formData: FormData,
 ) => {
   try {
+    // currentUser() について:
+    // https://clerk.com/docs/references/nextjs/current-user
+    const user = await currentUser();
+
+    if (!user) throw new Error("Please login to create a profile");
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+
     console.log(validatedFields);
     // { firstName: 'TOMOYA', lastName: 'WANIKO', username: 'alligatorfree' }
-    return { message: "profile created" };
-  } catch (e) {
-    console.log(e);
-    return { message: "there was an error" };
+
+    // updateUserMetadata() について:
+    // https://clerk.com/docs/references/backend/user/update-user-metadata
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: { hasProfile: true },
+    });
+  } catch (error) {
+    console.log(error);
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
   }
+
+  redirect("/");
+};
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: { clerkId: user.id },
+    select: { profileImage: true },
+  });
+
+  return profile?.profileImage;
 };

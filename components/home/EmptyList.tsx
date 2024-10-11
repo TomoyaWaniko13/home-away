9998d353412fa31ad9dda91b583c5869e0280030
tomoya <alligatorfree12@gmail.com>
2024-10-11 11:14:34 +0900

    98. Empty List

--- components/home/EmptyList.tsx
+++ components/home/EmptyList.tsx
@@ -1,6 +1,25 @@
 // 96. Home Page - Setup
-const EmptyList = () => {
-  return <div>EmptyList</div>;
+// 98. Empty List
+
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+type Props = {
+  heading?: string;
+  message?: string;
+  btnText?: string;
+};
+
+const EmptyList = ({ heading = 'No items in the list', message = 'keep exploring our properties', btnText = 'back home' }: Props) => {
+  return (
+    <div>
+      <h2 className={'text-xl font-bold'}>{heading}</h2>EmptyList
+      <p className={'text-lg'}>{message}</p>
+      <Button asChild className={'mt-4 capitalize'} size={'lg'}>
+        <Link href={'/'}>{btnText}</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default EmptyList;

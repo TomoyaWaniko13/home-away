fc784d092f0c863c781099d42b9bfef9446b2eae
tomoya <alligatorfree12@gmail.com>
2024-10-11 17:46:10 +0900

    105. Loading Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import CategoriesList from '@/components/home/CategoriesList';
-import PropertiesWrapper from '@/components/home/PropertiesWrapper';
-
 // 48. Remove Boilerplate
 // 96. Home Page - Setup
+// 105. Loading Cards
+
+import PropertiesWrapper from '@/components/home/PropertiesWrapper';
+import CategoriesList from '@/components/home/CategoriesList';
+import LoadingCards from '@/components/card/LoadingCards';
+import { Suspense } from 'react';
 
 type Props = {
   searchParams: { category?: string; search?: string };
@@ -12,7 +15,9 @@ export default function HomePage({ searchParams }: Props) {
   return (
     <section>
       <CategoriesList categoryQuery={searchParams.category} searchQuery={searchParams.search} />
-      <PropertiesWrapper categoryQuery={searchParams.category} searchQuery={searchParams.search} />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesWrapper categoryQuery={searchParams.category} searchQuery={searchParams.search} />
+      </Suspense>
     </section>
   );
 }
--- components/card/LoadingCards.tsx
+++ components/card/LoadingCards.tsx
@@ -1,7 +1,27 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
 // 100. PropertyCard - Setup
+// 105. Loading Cards
 
 const LoadingCards = () => {
-  return <div>LoadingCards</div>;
+  return (
+    <div className={'mt-4 gap-8 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'}>
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+      <SkeletonCard />
+    </div>
+  );
 };
+
+export function SkeletonCard() {
+  return (
+    <div>
+      <Skeleton className={'h-[300px] rounded-md'} />
+      <Skeleton className={'h-4 mt-2 w-3/4'} />
+      <Skeleton className={'h-4 mt-2 w-1/2'} />
+    </div>
+  );
+}
 
 export default LoadingCards;

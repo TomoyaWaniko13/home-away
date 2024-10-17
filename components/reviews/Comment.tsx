7e996f7b22c09d0da9721cf8696153baf752f768
tomoya <alligatorfree12@gmail.com>
2024-10-17 13:28:24 +0900

    130. ReviewCard Component

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,7 +1,35 @@
-// 124. Reviews Model
+'use client';
 
-const Comment = () => {
-  return <div>Comment</div>;
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+// 124. Reviews Model
+// 130. ReviewCard Component
+
+type Props = {
+  comment: string;
+};
+
+const Comment = ({ comment }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const longComment = comment.length > 130;
+  const displayComment = longComment && !isExpanded ? `${comment.slice(0, 130)}` : comment;
+
+  return (
+    <div>
+      <p>{displayComment}</p>
+      {longComment && (
+        <Button variant={'link'} className={'pl-0 text-muted-foreground'} onClick={toggleExpanded}>
+          {isExpanded ? 'Show less' : 'Show more'}
+        </Button>
+      )}
+    </div>
+  );
 };
 
 export default Comment;

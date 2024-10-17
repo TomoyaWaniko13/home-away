df8c8d2d134d71187bfc8971e5a6a985aae74789
tomoya <alligatorfree12@gmail.com>
2024-10-17 13:57:47 +0900

    131. Fetch and Delete User Reviews

--- actions/reviewAction.ts
+++ actions/reviewAction.ts
@@ -48,11 +48,32 @@ export const fetchPropertyReviews = async (propertyId: string) => {
 };
 
 // 124. Reviews Model
+// 131. Fetch and Delete User Reviews
 export const fetchPropertyReviewsByUser = async () => {
-  return { message: 'fetch user reviews' };
+  const user = await getAuthUser();
+
+  const reviews = await db.review.findMany({
+    where: { profileId: user.id },
+    select: { id: true, rating: true, comment: true, property: { select: { name: true, image: true } } },
+  });
+
+  return reviews;
 };
 
 // 124. Reviews Model
-export const deleteReviewAction = async () => {
-  return { message: 'delete reviews' };
+// 131. Fetch and Delete User Reviews
+export const deleteReviewAction = async (prevState: { reviewId: string }) => {
+  const { reviewId } = prevState;
+  const user = await getAuthUser();
+
+  try {
+    await db.review.delete({
+      where: { id: reviewId, profileId: user.id },
+    });
+
+    revalidatePath('/reviews');
+    return { message: 'Review deleted successfully' };
+  } catch (error) {
+    return renderError(error);
+  }
 };

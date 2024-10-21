63cb9f56b65d1f9e572fc1d576d44e9853384d2e
tomoya <alligatorfree12@gmail.com>
2024-10-22 05:25:26 +0900

    162. Admin Page - Loading Components

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,22 @@
+import { Suspense } from 'react';
+import StatsContainer from '@/components/admin/StatsContainer';
+import { ChartsLoadingContainer, StatsLoadingContainer } from '@/components/admin/Loading';
+import ChartsContainer from '@/components/admin/ChartsContainer';
+
 // 160. Admin Page - Setup
+// 162. Admin Page - Loading Components
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <>
+      <Suspense fallback={<StatsLoadingContainer />}>
+        <StatsContainer />
+      </Suspense>
+      <Suspense fallback={<ChartsLoadingContainer />}>
+        <ChartsContainer />
+      </Suspense>
+    </>
+  );
 };
 
 export default AdminPage;
--- components/admin/Loading.tsx
+++ components/admin/Loading.tsx
@@ -1,7 +1,29 @@
-// 160. Admin Page - Setup
+import { Card, CardHeader } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 
-const Loading = () => {
-  return <div>Loading</div>;
+// 160. Admin Page - Setup
+// 162. Admin Page - Loading Components
+
+const LoadingCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <Skeleton className={'w-full h-20 rounded'} />
+      </CardHeader>
+    </Card>
+  );
 };
 
-export default Loading;
+export const StatsLoadingContainer = () => {
+  return (
+    <div className={'mt-8 grid md:grid-cols-2 gap-4 lg:grid-cols-3'}>
+      <LoadingCard />
+      <LoadingCard />
+      <LoadingCard />
+    </div>
+  );
+};
+
+export const ChartsLoadingContainer = () => {
+  return <Skeleton className={'mt-16 w-full h-[300px] rounded'} />;
+};

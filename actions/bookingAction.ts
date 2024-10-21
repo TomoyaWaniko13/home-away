369f0881ca2a3b689776c5a7f2e83191285c6ddb
tomoya <alligatorfree12@gmail.com>
2024-10-21 13:18:01 +0900

    158. Fetch Reservations

--- actions/bookingAction.ts
+++ actions/bookingAction.ts
@@ -67,3 +67,16 @@ export const deleteBookingAction = async (prevState: { bookingId: string }) => {
     return renderError(error);
   }
 };
+
+// 158. Fetch Reservations
+export const fetchReservations = async () => {
+  const user = await getAuthUser();
+
+  const reservations = await db.booking.findMany({
+    where: { property: { profileId: user.id } },
+    orderBy: { createdAt: 'desc' },
+    include: { property: { select: { id: true, name: true, price: true, country: true } } },
+  });
+
+  return reservations;
+};

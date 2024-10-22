4e64ccd068d2a776e88157cb1aa444d29943f584
tomoya <alligatorfree12@gmail.com>
2024-10-22 12:40:29 +0900

    165. Admin Page - Chart Container

--- components/admin/Chart.tsx
+++ components/admin/Chart.tsx
@@ -1,7 +1,38 @@
-// 160. Admin Page - Setup
+'use client';
 
-const Chart = () => {
-  return <div>Chart</div>;
+// 160. Admin Page - Setup
+// 165. Admin Page - Chart Container
+
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
+
+const chartConfig = {
+  count: { label: 'Bookings', color: '#2563eb' },
+} satisfies ChartConfig;
+
+type Props = {
+  data: { date: string; count: number }[];
+};
+
+const Chart = ({ data }: Props) => {
+  console.log(data);
+
+  return (
+    <section className={'mt-24'}>
+      <h1 className={'text-4xl font-semibold text-center '}>Monthly Bookings</h1>
+      <ChartContainer config={chartConfig} className='min-h-[200px] w-full mt-10'>
+        <BarChart accessibilityLayer data={data}>
+          <CartesianGrid vertical={false} />
+          <XAxis dataKey='date' tickLine={true} tickMargin={3} axisLine={true} />
+          <YAxis allowDecimals={false} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <Bar dataKey='count' fill='var(--color-count)' radius={4} />
+        </BarChart>
+      </ChartContainer>
+    </section>
+  );
 };
 
 export default Chart;
--- components/admin/ChartsContainer.tsx
+++ components/admin/ChartsContainer.tsx
@@ -1,7 +1,15 @@
-// 160. Admin Page - Setup
+import { fetchChartsData } from '@/actions/adminAction';
+import Chart from '@/components/admin/Chart';
 
-const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+// 160. Admin Page - Setup
+// 165. Admin Page - Chart Container
+
+const ChartsContainer = async () => {
+  const bookings: { date: string; count: number }[] = await fetchChartsData();
+
+  if (bookings.length < 1) return null;
+
+  return <Chart data={bookings} />;
 };
 
 export default ChartsContainer;
